5222e1e66cb99259fa99e1ddd341b5a0a93e5c4b
sulg16 <mmopq2@naver.com>
2025-09-01 14:20:37 +0900

    [TEST] 동작확인
    .

--- Clicker/Assets/Script/ClickInputHandler.cs
+++ Clicker/Assets/Script/ClickInputHandler.cs
@@ -8,6 +8,9 @@ public class ClickInputHandler : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0)) // 클릭 감지
         {
+
+            Debug.Log("Valid Click!");
+
             // 1) UI 위인지 체크
             // 2) 게임 상태 체크 (Paused면 무시)
             // 3) 조건 통과 시 "유효 클릭 발생" 신호 발행
