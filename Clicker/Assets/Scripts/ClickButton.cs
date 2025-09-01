ec43ba716ea1c93dea695925f10d2cccc8c5130a
sulg16 <mmopq2@naver.com>
2025-09-01 14:08:23 +0900

    [Test] 동작테스트
    .

---
+++ Clicker/Assets/Scripts/ClickButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour
+{
+    public int gold = 0;
+    public int goldPerClick = 1;
+    public void OnClick()
+    {
+        gold = gold + goldPerClick;
+    }
+}
