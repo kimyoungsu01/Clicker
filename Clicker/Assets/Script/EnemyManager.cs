6839dd84a10f71a74dfdf3f0818d7a28dade1421
wlwhstnqls <wlwhstnqls5@gmail.com>
2025-09-03 17:45:38 +0900

    [fix]에너미 디스트로이&리스폰 , 에너미매니저추가

--- Clicker/Assets/Script/EnemyManager.cs
+++ Clicker/Assets/Script/EnemyManager.cs
@@ -5,11 +5,35 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     public static EnemyManager Instance;
-    public Enemy enemy;
+    [HideInInspector]public Enemy enemy;
 
     void Awake()
     {
       if(Instance == null) Instance = this;
       else Destroy(gameObject);
+    }
+
+    public void Respwan()
+    {
+        GameManager.Instance.stagecnt.UpdateStage();
+        GameObject monster = Resources.Load<GameObject>("Prefabs/LV1enemy");
+        enemy = Instantiate(monster, new Vector3(0, 0, 0), Quaternion.identity).GetComponent<Enemy>();
+    }
+
+    public void TurnoffUI()
+    {
+        if (WeaponManager.Instance.weaponUI.weaponInventoryUI.activeSelf == true)
+        {
+            gameObject.GetComponentInChildren<Canvas>().enabled = false;
+        }
+
+    }
+
+    public void TurnonUI()
+    {
+        if (WeaponManager.Instance.weaponUI.weaponInventoryUI.activeSelf == false)
+        {
+            gameObject.GetComponentInChildren<Canvas>().enabled = true;
+        }
     }
 }
--- Clicker/Assets/Script/Weapons/WeaponInventoryUI.cs
+++ Clicker/Assets/Script/Weapons/WeaponInventoryUI.cs
@@ -38,6 +38,6 @@ public class WeaponInventoryUI : MonoBehaviour
     public void BackButton()
     {
         gameObject.SetActive(false);
-        EnemyManager.Instance.enemy.TurnonUI();
+        EnemyManager.Instance.TurnonUI();
     }
 }
