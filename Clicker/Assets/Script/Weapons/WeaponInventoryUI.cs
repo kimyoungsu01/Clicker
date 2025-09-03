a1cd6c4b5c17c17b849f427b6f42eab1850f42b2
Seungmin7979 <safetmdals79@gmail.com>
2025-09-03 11:20:19 +0900

    [feat] WeaponInventory 닫기 버튼

--- Clicker/Assets/Script/Weapons/WeaponInventoryUI.cs
+++ Clicker/Assets/Script/Weapons/WeaponInventoryUI.cs
@@ -7,12 +7,16 @@ public class WeaponInventoryUI : MonoBehaviour
     [SerializeField] private Transform slotParent;
     [SerializeField] private GameObject slotPrefab;
 
+    [SerializeField] private Button backButton;
+
     private List<WeaponData> weaponList = new List<WeaponData>();
 
     private void Start()
     {
         LoadAllWeapons();
         CreateInventoryUI();
+
+        backButton.onClick.AddListener(BackButton);
     }
 
     // Resources 폴더에서 모든 무기 불러오기
@@ -28,18 +32,11 @@ public class WeaponInventoryUI : MonoBehaviour
         foreach (WeaponData weapon in weaponList)
         {
             GameObject slot = Instantiate(slotPrefab, slotParent);
-
-            // 슬롯 UI 구성 요소 가져오기
-            //Image icon = slot.transform.Find("Icon").GetComponent<Image>();
-            //Text nameText = slot.transform.Find("Name").GetComponent<Text>();
-            //Button buyButton = slot.transform.Find("BuyButton").GetComponent<Button>();
-
-            // 무기 데이터 반영
-            //icon.sprite = weapon.weaponIcon;
-            //nameText.text = weapon.weaponName;
-
-            // 버튼에 무기 연결
-            // buyButton.onClick.AddListener(() => OnClickBuy(weapon));
         }
+    }
+
+    public void BackButton()
+    {
+        gameObject.SetActive(false);
     }
 }
