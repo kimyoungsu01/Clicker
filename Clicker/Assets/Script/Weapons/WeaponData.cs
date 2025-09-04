9867650f77face990b5b5777e2761d4b2b865485
Seungmin7979 <safetmdals79@gmail.com>
2025-09-04 21:02:54 +0900

    [feat] 크리 데미지 구현

--- Clicker/Assets/Script/Weapons/WeaponData.cs
+++ Clicker/Assets/Script/Weapons/WeaponData.cs
@@ -12,7 +12,7 @@ public class WeaponData : ScriptableObject
 
     public int atkDmgIncreasePerLevel;     // 공격력 강화당 증가량
     public int criRateIncreasePerLevel;    // 크리티컬 강화당 증가량
-
+    
     public int buyCost;               // 무기 구매 비용
     public int baseUpgradeCost;       // 처음 강화 비용
 }
