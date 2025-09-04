84526b417acbc316b41f7ea0f6f9045a6a6cab88
kimyoungsu01 <kysmb200@gmail.com>
2025-09-04 21:51:06 +0900

    [fix] 플레이어 데이타 무기 업그레이드 추가

--- Clicker/Assets/Script/Data/PlayerData.cs
+++ Clicker/Assets/Script/Data/PlayerData.cs
@@ -8,6 +8,7 @@ public class PlayerData
     public int weaponData;
     public int weaponLv;
     public PlayerStat playerStat;
+    public int[] upgradeLevels = new int[5];
 
     // 생성자란? 형식: 반환타입x, 클래스랑 이름이 똑같다,
     // 매개변수는 있어도 되고 없어도 된다,
@@ -20,5 +21,11 @@ public class PlayerData
         weaponData = Data;
         weaponLv = lv;
         this.playerStat = playerStat;
+    }
+
+    public void weapons() 
+    {
+        WeaponManager.Instance.upgradeLevels = new int[5];
+        return;
     }
 }
