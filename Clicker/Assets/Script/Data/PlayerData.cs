e24fb708236da3be2827bea77769749e6dec3a77
kimyoungsu01 <kysmb200@gmail.com>
2025-09-04 19:46:42 +0900

    [feat] 화면 cost 추가

--- Clicker/Assets/Script/Data/PlayerData.cs
+++ Clicker/Assets/Script/Data/PlayerData.cs
@@ -5,23 +5,21 @@ public class PlayerData
     public int goldCount;
     public int pointCount;
     public int stageID;
-    public string monsterName;
-    public int monsterHPCount;
     public int atxCount;
     public int ctcCount;
+    public int ctcDamageCount;
 
     // 생성자란? 형식: 반환타입x, 클래스랑 이름이 똑같다,
     // 매개변수는 있어도 되고 없어도 된다,
     // 생성자의 맴버변수가 매개변수로 사용가능
-    public PlayerData(int id, string name, int hp, int atx, int ctc, int gold, int point)
+    public PlayerData(int id, int atx, int ctc, int ctcDamage, int gold, int point)
     {
         stageID = id;
-        monsterName = name;
-        monsterHPCount = hp;
         atxCount = atx;
         ctcCount = ctc;
         goldCount = gold;
         pointCount = point;
+        ctcDamageCount = ctcDamage;
     }
 
     //public  static PlayerData CreateDefaultPlayer(int Id, string IdName)
--- Clicker/Assets/Script/Manager/GameManager.cs
+++ Clicker/Assets/Script/Manager/GameManager.cs
@@ -63,7 +63,7 @@ public class GameManager : MonoBehaviour
         {
             // 스테이지 (넘버) , 몬스터 (이름, hp)
             // 플레이어 (공격력, 크리티컬, 포인트, 골드) 
-            playerData = new PlayerData(0,"", 0, 0, 0, 1000, 1000);
+            playerData = new PlayerData(0, 0, 0, 0, 1000, 1000);
             Debug.Log(playerData);
         }
     }
